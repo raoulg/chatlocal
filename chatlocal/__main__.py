b867d1a8dd887a15cc41bc40fcc75db2180e8b28
Raoul Grouls <Raoul.Grouls@han.nl>
2023-09-21 16:20:42 +0200

    script to toml

---
+++ chatlocal/__main__.py
@@ -0,0 +1,42 @@
+
+
+from dataloader import read_toml
+from loguru import logger
+
+from chatlocal import DataLoader, FileType, ModelType, VectorStore, VectorStoreSettings
+from chatlocal.settings import UserConfig
+
+
+def build_vectorstore() -> None:
+    userconfig = UserConfig(**read_toml())
+    logger.info(f"using folder {userconfig.folder} to build vectorstore.")
+
+    filetypes = [FileType(ft) for ft in userconfig.filetypes]
+    logger.info(f"Looking for filetypes {[ft.value for ft in filetypes]}.")
+
+    dataloader = DataLoader(filetypes=filetypes)
+    dataloader.load_files(userconfig.folder)
+    logger.success("Finished loading files")
+
+    storesettings = VectorStoreSettings(
+        chunk_size=userconfig.chunk_size,
+        separator="\n",
+        store_file=userconfig.store_file,
+        modeltype=ModelType.OPENAI,
+    )
+
+    vectorstore = VectorStore(settings=storesettings)
+    if vectorstore.store_path.exists():
+        logger.info(f"Found existing vectorstore at {vectorstore.store_path}")
+        logger.info("Extending vectorstore with all files found in folder.")
+        vectorstore.extend_store(dataloader.get_datastore())
+    else:
+        logger.info(
+            f"No existing vectorstore found at {vectorstore.store_path}"
+            "Creating new vectorstore.",
+            )
+
+        vectorstore.create_store(dataloader.get_datastore())
+    vectorstore.save()
+    logger.success(f"Saved vectorstore to {vectorstore.store_path}")
+
